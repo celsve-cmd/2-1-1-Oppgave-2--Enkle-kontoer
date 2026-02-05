932700686dea3b349763e7297e1bb4c8e42436ab
celsve-cmd <celsve@gmail.com>
2026-02-05 14:44:17 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Lag et objekt som representerer en bankkonto med:
+navn
+saldo
+Legg til en metode endreSaldo(beløp) som:
+bruker this for å oppdatere saldoen
+bruker if/else til å sjekke om saldoen blir negativ: hvis ja, vis advarsel; hvis nei, oppdater saldoen
+vis resultatet på siden*/
+
+
