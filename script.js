2f0d898be989df81d441c52bb9995a76c92021a4
celsve-cmd <celsve@gmail.com>
2026-02-12 14:47:56 +0100

    -

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 /*Lag et objekt som representerer en bankkonto med:
-navn
-saldo
-Legg til en metode endreSaldo(beløp) som:
-bruker this for å oppdatere saldoen
-bruker if/else til å sjekke om saldoen blir negativ: hvis ja, vis advarsel; hvis nei, oppdater saldoen
-vis resultatet på siden*/
+   navn
+   saldo
+   Legg til en metode endreSaldo(beløp) som:
+   bruker this for å oppdatere saldoen
+   bruker if/else til å sjekke om saldoen blir negativ: hvis ja, vis advarsel; hvis nei, oppdater saldoen
+   vis resultatet på siden*/
 
 
+
+
+let navn = ''
+let password = ''
+
+let correctName = 'Navn Navnesen'
+let correctPassword = 'SterktPassord123.'
+
+
+
+
+updateView()
+function updateView() {
+    document.getElementById('app').innerHTML = /*HTML*/ `
+            username: <input onchange = "setUserName(this.value)">
+            <br>
+            password: <input onchange = "setPassword(this.value)">
+            <br>
+            <button onclick ="logIn()">Logg in</button>
+            `
+}
+
+
+function setUserName(userInput) {
+    userName = userInput
+}
+
+function setPassword(passwordInput) {
+    userPassword = passwordInput
+}
+
+
+function logIn() {
+if(navn == correctName){
+console.log('Hei!')
+}
+}
