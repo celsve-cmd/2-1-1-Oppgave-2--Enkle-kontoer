7808d401fc489219716d6790848e5a79636d9014
celsve-cmd <celsve@gmail.com>
2026-02-13 14:59:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,28 +7,25 @@
    vis resultatet på siden*/
 
 
+const siden = document.getElementById('app')
 
+let correctName = 'Navn'
+let correctPassword = '123'
 
-let navn = ''
-let password = ''
-
-let correctName = 'Navn Navnesen'
-let correctPassword = 'SterktPassord123.'
-
+let balance = 0
 
 
 
-updateView()
-function updateView() {
-    document.getElementById('app').innerHTML = /*HTML*/ `
+logInPage()
+function logInPage() {
+    siden.innerHTML = /*HTML*/ `
             username: <input onchange = "setUserName(this.value)">
             <br>
-            password: <input onchange = "setPassword(this.value)">
+            password: <input type="password" onchange = "setPassword(this.value)">
             <br>
-            <button onclick ="logIn()">Logg in</button>
+            <button onclick ="logIn()">Logg inn</button>
             `
 }
-
 
 function setUserName(userInput) {
     userName = userInput
@@ -38,9 +35,44 @@ function setPassword(passwordInput) {
     userPassword = passwordInput
 }
 
-
 function logIn() {
-if(navn == correctName){
-console.log('Hei!')
+    if (userName == correctName && userPassword == correctPassword) {
+        loggedInPage()
+    } else {
+        alert('Feil brukernavn eller passord!')
+    }
 }
+
+function loggedInPage() {
+    siden.innerHTML = /*HTML*/`
+    <div>Hei, ${correctName}!</div>
+    <div id="saldo">Din saldo er ${balance}</div>
+    <input id="money">
+    <br>
+    <button id="addMoneyBtn" onclick ="addMoney()">Legg til penger</button>
+    <br>
+    <button id="removeMoneyBtn" onclick ="removeMoney()">Trekk fra penger</button>
+    <br>
+    <button id="logOut" onclick = "logOut()">Logg ut</button>`
+}
+
+function addMoney() {
+    let input = document.getElementById("money").value
+    balance += Number(input)
+
+    document.getElementById("saldo").innerHTML =
+        `Din saldo er ${balance} kr`
+}
+
+function removeMoney(){
+    let input = document.getElementById("money").value
+    balance -= Number(input)
+    document.getElementById("saldo").innerHTML =
+        `Din saldo er ${balance} kr`
+}
+
+function logOut() {
+    userName = ''
+    userPassword = ''
+    logInPage()
 }
